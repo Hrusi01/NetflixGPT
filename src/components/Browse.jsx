fbe0d5bc99ca39f82aad61e785fd91acf7b01be9
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2023-12-19 22:28:33 +0530

    useNowPlayingMovies hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants"
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header"
-import { useDispatch } from "react-redux";
-import { addNowPlyingMovies } from "../utils/moviesSlice";
-
 const Browse = () => {
 
-    const dispatch = useDispatch();
-
-    const getNowPlaying = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS); //return some promises
-        const json = await data.json();
-        console.log(json.results);
-        dispatch(addNowPlyingMovies(json.results));
-    };
-
-    useEffect(() => {
-        getNowPlaying();
-    }, [])
+    useNowPlayingMovies();
 
     return (
         <div className="text-xl">
