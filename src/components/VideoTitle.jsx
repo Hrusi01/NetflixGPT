180a7fff0ecfbf83010f8bd420e0f134c272cea2
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2023-12-23 14:48:21 +0530

    bg-video

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,18 +1,22 @@
 import playIcon from "../img/play-button-icon.jpg"
+import { GoTriangleRight } from "react-icons/go";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
+
 const VideoTitle = ({ title, overview }) => {
     return (
-        <div className="pt-36 px-16">
+        <div className="w-screen aspect-video pt-[20%] px-16 absolute bg-gradient-to-r from-black text-white">
             <h1 className="text-3xl font-bold">
                 {title}
             </h1>
             <p className="py-6 text-lg w-1/3">
                 {overview}
             </p>
-            <div className="">
-                <button className="bg-gray-500 py-2 px-7 text-white justify-center bg-opacity-50 rounded-sm items-center text-center">
-                    &#9654;
-                    Play</button>
-                <button className=" mx-2 bg-gray-500 py-2 px-7 text-white justify-center bg-opacity-50 rounded-sm items-center text-center">More Info</button>
+            <div className="flex">
+                <button className="bg-white text-black py-2 px-7 justify-center rounded-md items-center text-center flex mx-2 hover:bg-opacity-80">
+                    <GoTriangleRight className="scale-[2]" /> Play
+                </button>
+                <button className="bg-gray-500 py-2 px-4 text-white justify-center bg-opacity-50 rounded-md items-center text-center flex hover:bg-opacity-80"> <IoIosInformationCircleOutline className=" m-1 scale-[1.3]" /> More Info</button>
             </div>
         </div>
     )
