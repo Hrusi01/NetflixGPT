439dd48d3afacb8ebd84af2199d3d4b69b88d99d
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2024-01-15 23:46:10 +0530

    key is secured

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTlmOGQ5NmQyMWJjOTE5YzYxMjJjZTFjNTg3ZTE1MSIsInN1YiI6IjY1ODE4MDQxZDUxOTFmMDhjMGFlMDJlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tJw75aGEOTfPkUM48IE1bTT2ke8_UyvuFVBjQ0qNVdw'
+        Authorization: 'Bearer' + process.env.REACR_APP_API_OPTIONS
     }
 
 }
@@ -18,4 +18,4 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 export const BG_URL =
     "https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c5af601a-6657-4531-8f82-22e629a3795e/IN-en-20231211-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-export const OPEN_AI_KEY = "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"   
+export const OPEN_AI_KEY = process.env.REACR_APP_OPEN_AI_KEY
